55e50e23f426dfd8996ef9339c89d550a8bbce9d
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2021-04-18 17:25:22 +0200

    Add initial unittests (mostly framework)

---
+++ tests/helpers.py
@@ -0,0 +1,20 @@
+"""Test helpers for Philips Hue Play HDMI Sync Box."""
+import pytest
+
+from pytest_homeassistant_custom_component.common import AsyncMock, Mock, patch
+
+
+@pytest.fixture
+def mock_api(hass):
+    """Mock the Philips Hue Play HDMI Sync Box api."""
+    api = Mock(initialize=AsyncMock(return_value={""}), register=AsyncMock(), close=AsyncMock())
+    api.mock_requests = []
+
+    async def mock_request(method, path, **kwargs):
+        kwargs["method"] = method
+        kwargs["path"] = path
+        api.mock_requests.append(kwargs)
+
+        return None
+
+    return api
---
+++ tests/test_huesyncbox.py
@@ -0,0 +1,52 @@
+"""Test the Philips Hue Play HDMI Sync Box class."""
+
+import pytest
+
+import aiohuesyncbox, asyncio
+
+from pytest_homeassistant_custom_component.common import patch
+# from pytest_homeassistant_custom_component.common import (
+#     MockConfigEntry,
+# )
+
+from .helpers import mock_api
+
+from custom_components.huesyncbox import huesyncbox, errors
+
+# TODO: move device registry code from huesyncbox class to async_setup_entry in  __init__.py
+# async def test_huesyncbox_setup(hass, mock_api):
+#     """Test a successful setup."""
+#     entry = MockConfigEntry()
+#     entry.data = {"host": "1.2.3.4", "unique_id": "mock-unique-id", "access_token": "mock-access-token", "port": 1234, "path": "mock-path"}
+
+#     syncbox = huesyncbox.HueSyncBox(hass, entry)
+
+#     with patch("aiohuesyncbox.HueSyncBox", return_value=mock_api):
+#         assert await syncbox.async_setup() is True
+
+#     assert syncbox.api is mock_api
+#     assert mock_api.initialize.call_count == 1
+
+async def test_register_authentication_required(hass, mock_api):
+    """Test exception if we cannot connect."""
+    with patch.object(
+        mock_api, "register", side_effect=asyncio.TimeoutError
+    ), pytest.raises(huesyncbox.AuthenticationRequired):
+        await huesyncbox.async_register_aiohuesyncbox(hass, mock_api)
+
+    with patch.object(
+        mock_api, "register", side_effect=aiohuesyncbox.Unauthorized
+    ), pytest.raises(huesyncbox.AuthenticationRequired):
+        await huesyncbox.async_register_aiohuesyncbox(hass, mock_api)
+
+async def test_register_cannot_connect(hass, mock_api):
+    """Test cannot connect exception."""
+    with patch.object(
+        mock_api, "register", side_effect=aiohuesyncbox.RequestError
+    ), pytest.raises(huesyncbox.CannotConnect):
+        await huesyncbox.async_register_aiohuesyncbox(hass, mock_api)
+
+    with patch.object(
+        mock_api, "register", side_effect=aiohuesyncbox.AiohuesyncboxException
+    ), pytest.raises(huesyncbox.CannotConnect):
+        await huesyncbox.async_register_aiohuesyncbox(hass, mock_api)
