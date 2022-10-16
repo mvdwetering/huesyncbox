88521e21caa54444db8c2a6bc3d6824daa5ae2ab
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2022-10-16 18:40:17 +0200

    Simplify platform setup

--- custom_components/huesyncbox/__init__.py
+++ custom_components/huesyncbox/__init__.py
@@ -124,10 +124,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     if not await huesyncbox.async_setup():
         return False
 
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, platform)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     # Register services on first entry
     global services_registered
