c5b1ab1c560bced935effd408fb6b562596466c7
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2023-07-28 21:24:56 +0200

    Add DUC and Switch platform

--- custom_components/huesyncbox/__init__.py
+++ custom_components/huesyncbox/__init__.py
@@ -1,18 +1,59 @@
 """The Philips Hue Play HDMI Sync Box integration."""
 
+from datetime import timedelta
 import aiohuesyncbox
+import async_timeout
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryAuthFailed, ConfigEntryError
 from homeassistant.helpers import device_registry
+from homeassistant.helpers.update_coordinator import (
+    DataUpdateCoordinator,
+    UpdateFailed,
+)
 
-from .const import DOMAIN, MANUFACTURER_NAME
+from .const import DOMAIN, MANUFACTURER_NAME, LOGGER
 
 # TODO List the platforms that you want to support.
 # For your initial PR, limit it to 1 platform.
-PLATFORMS: list[Platform] = []
+PLATFORMS: list[Platform] = [Platform.SWITCH]
+
+
+class HueSyncBoxCoordinator(DataUpdateCoordinator):
+    """My custom coordinator."""
+
+    def __init__(self, hass, api: aiohuesyncbox.HueSyncBox):
+        """Initialize my coordinator."""
+        super().__init__(
+            hass,
+            LOGGER,
+            # Name of the data. For logging purposes.
+            name="HueSyncBox data",
+            # Polling interval. Will only be polled if there are subscribers.
+            update_interval=timedelta(seconds=2),
+        )
+        self.api = api
+
+    async def _async_update_data(self):
+        """Fetch data from API endpoint.
+
+        This is the place to pre-process the data to lookup tables
+        so entities can quickly look up their data.
+        """
+        try:
+            # Note: asyncio.TimeoutError and aiohttp.ClientError are already
+            # handled by the data update coordinator.
+            async with async_timeout.timeout(5):
+                await self.api.update()
+                return self.api
+        except aiohuesyncbox.Unauthorized as err:
+            # Raising ConfigEntryAuthFailed will cancel future updates
+            # and start a config flow with SOURCE_REAUTH (async_step_reauth)
+            raise ConfigEntryAuthFailed from err
+        except aiohuesyncbox.RequestError as err:
+            raise UpdateFailed(f"Error communicating with API: {err}")
 
 
 async def update_device_registry(
@@ -29,6 +70,7 @@ async def update_device_registry(
         model=api.device.device_type,
         sw_version=api.device.firmware_version,
     )
+
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Philips Hue Play HDMI Sync Box from a config entry."""
@@ -50,10 +92,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     except aiohuesyncbox.RequestError as err:
         raise ConfigEntryError(err) from err
 
-    hass.data[DOMAIN][entry.entry_id] = api
 
     await update_device_registry(hass, entry, api)
 
+    coordinator = HueSyncBoxCoordinator(hass, api)
+
+    hass.data[DOMAIN][entry.entry_id] = coordinator
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
--- custom_components/huesyncbox/const.py
+++ custom_components/huesyncbox/const.py
@@ -1,6 +1,8 @@
 """Constants for the Philips Hue Play HDMI Sync Box integration."""
+import logging
 
 DOMAIN = "huesyncbox"
+LOGGER = logging.getLogger(__package__)
 
 DEFAULT_PORT = 443
 
