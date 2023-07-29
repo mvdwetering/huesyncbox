5c8d95deb6e549257ff4a3b15ae7bc7b62638284
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2023-07-29 21:49:16 +0200

    Add set_bridge service

--- custom_components/huesyncbox/__init__.py
+++ custom_components/huesyncbox/__init__.py
@@ -1,16 +1,24 @@
 """The Philips Hue Play HDMI Sync Box integration."""
+import aiohuesyncbox
+import voluptuous as vol  # type: ignore
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryAuthFailed, ConfigEntryError
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.config_validation import make_entity_service_schema
+from homeassistant.helpers.service import async_extract_config_entry_ids
 
-
-from .const import DOMAIN
+from .const import (
+    ATTR_BRIDGE_CLIENTKEY,
+    ATTR_BRIDGE_ID,
+    ATTR_BRIDGE_USERNAME,
+    DOMAIN,
+    SERVICE_SET_BRIDGE,
+)
 from .coordinator import HueSyncBoxCoordinator
 from .helpers import update_device_registry
-
-import aiohuesyncbox
 
 PLATFORMS: list[Platform] = [
     Platform.NUMBER,
@@ -18,6 +26,47 @@ PLATFORMS: list[Platform] = [
     Platform.SENSOR,
     Platform.SWITCH,
 ]
+
+
+HUESYNCBOX_SET_BRIDGE_SCHEMA = make_entity_service_schema(
+    {
+        vol.Required(ATTR_BRIDGE_ID): cv.string,
+        vol.Required(ATTR_BRIDGE_USERNAME): cv.string,
+        vol.Required(ATTR_BRIDGE_CLIENTKEY): cv.string,
+    }
+)
+
+
+async def async_register_services(hass: HomeAssistant):
+    async def async_set_bridge(call):
+        """
+        Set bridge, note that this change is not instant.
+        After calling you will have to wait until the `bridge_unique_id` matches the new bridge id
+        and the bridge_connection_state is `connected`, `invalidgroup` or `streaming`, other status means it is connecting.
+        I have seen the bridge change to take around 15 seconds.
+        """
+
+        config_entry_ids = await async_extract_config_entry_ids(hass, call)
+        for config_entry_id in config_entry_ids:
+            coordinator = hass.data[DOMAIN][config_entry_id]
+
+            bridge_id = call.data.get(ATTR_BRIDGE_ID)
+            username = call.data.get(ATTR_BRIDGE_USERNAME)
+            clientkey = call.data.get(ATTR_BRIDGE_CLIENTKEY)
+
+            await coordinator.api.hue.set_bridge(bridge_id, username, clientkey)
+
+    if not hass.services.has_service(DOMAIN, SERVICE_SET_BRIDGE):
+        hass.services.async_register(
+            DOMAIN,
+            SERVICE_SET_BRIDGE,
+            async_set_bridge,
+            schema=HUESYNCBOX_SET_BRIDGE_SCHEMA,
+        )
+
+
+async def async_unregister_services(hass: HomeAssistant):
+    hass.services.async_remove(DOMAIN, SERVICE_SET_BRIDGE)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -47,13 +96,19 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data[DOMAIN][entry.entry_id] = coordinator
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
+    await async_register_services(hass)
+
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        api = hass.data[DOMAIN].pop(entry.entry_id)
-        await api.close()
+        coordinator = hass.data[DOMAIN].pop(entry.entry_id)
+        await coordinator.api.close()
+
+        if len(hass.data[DOMAIN]) == 0:
+            hass.data.pop(DOMAIN)
+            await async_unregister_services(hass)
 
     return unload_ok
--- custom_components/huesyncbox/const.py
+++ custom_components/huesyncbox/const.py
@@ -20,3 +20,10 @@ SYNC_MODE_MUSIC = "music"
 SYNC_MODE_GAME = "game"
 
 SYNC_MODES = [SYNC_MODE_VIDEO, SYNC_MODE_MUSIC, SYNC_MODE_GAME]
+
+
+SERVICE_SET_BRIDGE = "set_bridge"
+
+ATTR_BRIDGE_ID = "bridge_id"
+ATTR_BRIDGE_USERNAME = "bridge_username"
+ATTR_BRIDGE_CLIENTKEY = "bridge_clientkey"
